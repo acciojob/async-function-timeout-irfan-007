7f0aab257b8af81fbbebce1ece7c8d7400484fb9
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-23 20:25:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const text=document.getElementById("text").value;
+const delay=document.getElementById("delay").value;
+const btn=document.getElementById("btn");
+const output=document.getElementById("output");
+
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	myFunc();
+});
+
+async function myFunc(){
+	let promis=new Promis((resolve,reject)=>{
+		setTimeout(()=>output.innerHTML=text,delay);
+		return resolve("done!");
+	});
+	let result=await promis;
+	console.log(result);
+}
